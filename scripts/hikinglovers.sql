f91b70741fa4a8b1a37ef477f3e5ea7bd2dea3fd
cedricwietkin <wietkin.c@gmail.com>
2023-08-14 10:00:18 +0200

    fichier.sql

---
+++ scripts/hikinglovers.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS `hikinglovers` DEFAULT CHARACTER SET latin1;
+
+USE `hikinglovers`;
+
+DROP TABLE IF EXISTS `Hikes`;
+
+CREATE TABLE `Hikes` (
+                         `id` INT AUTO_INCREMENT PRIMARY KEY,
+                         `username` VARCHAR(100) NOT NULL,
+                         `distance` VARCHAR(180) NOT NULL,
+                         `duration` VARCHAR(255),
+                         `elevation_gain` DECIMAL(10, 2),
+                         `description` TEXT,
+                         `created_at` TIMESTAMP,
+                         `updated_at` TIMESTAMP
+);
+
+DROP TABLE IF EXISTS `Users`;
+
+CREATE TABLE `Users` (
+                         `id` INT AUTO_INCREMENT PRIMARY KEY,
+                         `firstname` VARCHAR(100) NOT NULL,
+                         `lastname` VARCHAR(100) NOT NULL,
+                         `nickname` VARCHAR(100) NOT NULL,
+                         `email` VARCHAR(180) NOT NULL,
+                         `password` VARCHAR(255),
+                         CONSTRAINT email UNIQUE (`email`),
+                         CONSTRAINT nickname UNIQUE (`nickname`)
+);
