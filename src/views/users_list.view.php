d8852f60fc1562d62aecdce1bcc1d3522e676f15
cedricwietkin <wietkin.c@gmail.com>
2023-08-17 13:59:44 +0200

    test

--- src/views/users_list.view.php
+++ src/views/users_list.view.php
@@ -5,18 +5,33 @@
         <button type="submit">Search</button>
     </form>
 
-    <div class="listing_user">
-        <ul>
-            <?php foreach ($users as $user): ?>
-                <li>
-                    <?= $user['firstname'] . ' ' . $user['lastname']; ?>
-                    <div>
-                        <buttton class="curd_user">Add+</buttton>
-                        <buttton class="curd_user">Send a message</buttton>
-                    </div>
-                
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
+    <?php if (!empty($_GET['search_query'])): ?>
+        <div class="listing_user">
+            <ul>
+                <?php foreach ($users as $user): ?>
+                    <li>
+                        <?= $user['firstname'] . ' ' . $user['lastname']; ?>
+                        <div>
+                            <button class="curd_user">Add+</button>
+                            <button class="curd_user">Send a message</button>
+                        </div>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php else: ?>
+        <div class="listing_user">
+            <ul>
+                <?php foreach ($users as $user): ?>
+                    <li>
+                        <?= $user['firstname'] . ' ' . $user['lastname']; ?>
+                        <div>
+                            <button class="curd_user">Add+</button>
+                            <button class="curd_user">Send a message</button>
+                        </div>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endif; ?>
 </div>
