1ac1a2723c08e4ac7d4f31231a25e619186d6f48
cedricwietkin <wietkin.c@gmail.com>
2023-08-18 12:25:29 +0200

    up

--- src/views/hike.view.php
+++ src/views/hike.view.php
@@ -1,12 +1,12 @@
-<h2><?= $hike['name']?></h2>
 
-<p>Description: <?= $hike['description']?> </p>
-<p>Distance: <?= $hike['distance'] ?></p>
-<p>Duration: <?= $hike['duration']?></p>
-<p>Elavation gain: <?= $hike['elevation_gain'] ?> </p>
-<p>Created by: <?= $hike['']?> </p>
-<p>Created at: <?= $hike['created_at']?> </p>
-<p>Uptaded at: <?= $hike['updated_at']?> </p>
+<h2><?= $hikes['name'];?></h2>
+<p>Description: <?= $hikes['description'];?> </p>
+<p>Distance: <?= $hikes['distance'] ?></p>
+<p>Duration: <?= $hikes['duration']?></p>
+<p>Elavation gain: <?= $hikes['elevation_gain'] ?> </p>
+<p>Created by: <?= $hikes['']?> </p>
+<p>Created at: <?= $hikes['created_at']?> </p>
+<p>Uptaded at: <?= $hikes['updated_at']?> </p>
 
 
 <a href="/"> Back to Homepage </a>
