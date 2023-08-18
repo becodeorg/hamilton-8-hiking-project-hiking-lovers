884f8e056a13744426635f78254d9fd7653269bc
cedricwietkin <wietkin.c@gmail.com>
2023-08-18 12:23:04 +0200

    up

---
+++ src/models/Hike.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Models;
+
+use PDO;
+
+
+class Hike extends Database
+{
+    public function findAll(int $limit = 0): array
+    {
+        $sql = "SELECT * FROM Hikes LIMIT 20";
+
+        $stmt = $this->query($sql);
+        $hikes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $hikes;
+    }
+
+    public function find(string $id): array|false
+    {
+        $stmt = $this->query(
+            "SELECT * FROM Hikes WHERE id = ?",
+            [$id]
+        );
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
